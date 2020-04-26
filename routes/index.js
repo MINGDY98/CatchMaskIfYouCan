68eed4973355b4f081787c2672a61708ff0f57bb
minjikim <alswl1500@naver.com>
2020-04-26 18:42:45 +0900

    처음세팅

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Catch Mask If You Can' });
+});
+
+module.exports = router;
